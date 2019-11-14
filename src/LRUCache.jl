1c98533910925d51a8a2e6404d7329f0b7e199d2
Jutho <Jutho@users.noreply.github.com>
2019-11-14 23:57:29 +0100

    switch to lock() do syntax (#15)
    * switch to lock() do syntax
    
    * bump version [ci skip]

--- src/LRUCache.jl
+++ src/LRUCache.jl
@@ -38,66 +38,61 @@ end
 Base.length(lru::LRU) = length(lru.keyset)
 Base.isempty(lru::LRU) = isempty(lru.keyset)
 function Base.sizehint!(lru::LRU, n::Integer)
-    lock(lru.lock)
-    sizehint!(lru.dict, n)
-    unlock(lru.lock)
+    lock(lru.lock) do
+        sizehint!(lru.dict, n)
+    end
     return lru
 end
 
 _unsafe_haskey(lru::LRU, key) = haskey(lru.dict, key)
 function Base.haskey(lru::LRU, key)
-    lock(lru.lock)
-    b = _unsafe_haskey(lru, key)
-    unlock(lru.lock)
-    return b
+    lock(lru.lock) do
+        return _unsafe_haskey(lru, key)
+    end
 end
 function Base.get(lru::LRU, key, default)
-    lock(lru.lock)
-    if _unsafe_haskey(lru, key)
-        v = _unsafe_getindex(lru, key)
-        unlock(lru.lock)
-        return v
-    else
-        unlock(lru.lock)
-        return default
+    lock(lru.lock) do
+        if _unsafe_haskey(lru, key)
+            v = _unsafe_getindex(lru, key)
+            return v
+        else
+            return default
+        end
     end
 end
 function Base.get(default::Callable, lru::LRU, key)
-    lock(lru.lock)
-    if _unsafe_haskey(lru, key)
-        v = _unsafe_getindex(lru, key)
-        unlock(lru.lock)
-        return v
-    else
-        unlock(lru.lock)
-        return default()
+    lock(lru.lock) do
+        if _unsafe_haskey(lru, key)
+            v = _unsafe_getindex(lru, key)
+            return v
+        else
+            return default()
+        end
     end
 end
 function Base.get!(lru::LRU, key, default)
-    lock(lru.lock)
-    if _unsafe_haskey(lru, key)
-        v = _unsafe_getindex(lru, key)
-        unlock(lru.lock)
+    lock(lru.lock) do
+        if _unsafe_haskey(lru, key)
+            v = _unsafe_getindex(lru, key)
+            return v
+        end
+        v = default
+        _unsafe_addindex!(lru, v, key)
+        _unsafe_resize!(lru)
         return v
     end
-    v = default
-    _unsafe_addindex!(lru, v, key)
-    _unsafe_resize!(lru)
-    unlock(lru.lock)
-    return v
 end
 function Base.get!(default::Callable, lru::LRU, key)
-    lock(lru.lock)
-    if _unsafe_haskey(lru, key)
-        v = _unsafe_getindex(lru, key)
-        unlock(lru.lock)
+    lock(lru.lock) do
+        if _unsafe_haskey(lru, key)
+            v = _unsafe_getindex(lru, key)
+            return v
+        end
+        v = default()
+        _unsafe_addindex!(lru, v, key)
+        _unsafe_resize!(lru)
         return v
     end
-    v = default()
-    _unsafe_addindex!(lru, v, key)
-    _unsafe_resize!(lru)
-    unlock(lru.lock)
-    return v
 end
 
 function _unsafe_getindex(lru::LRU, key)
@@ -106,14 +101,13 @@ function _unsafe_getindex(lru::LRU, key)
     return v
 end
 function Base.getindex(lru::LRU, key)
-    lock(lru.lock)
-    if _unsafe_haskey(lru, key)
-        v = _unsafe_getindex(lru, key)
-        unlock(lru.lock)
-        return v
-    else
-        unlock(lru.lock)
-        throw(KeyError(key))
+    lock(lru.lock) do
+        if _unsafe_haskey(lru, key)
+            v = _unsafe_getindex(lru, key)
+            return v
+        else
+            throw(KeyError(key))
+        end
     end
 end
 function _unsafe_addindex!(lru::LRU{K}, v, key) where K
@@ -124,19 +118,19 @@ function _unsafe_addindex!(lru::LRU{K}, v, key) where K
     lru.dict[key] = (v, n, s)
 end
 function Base.setindex!(lru::LRU, v, key)
-    lock(lru.lock)
-    if _unsafe_haskey(lru, key)
-        _, n, s = lru.dict[key]
-        lru.currentsize -= s
-        s = lru.by(v)::Int
-        lru.currentsize += s
-        lru.dict[key] = (v, n, s)
-        _move_to_front!(lru.keyset, n)
-    else
-        _unsafe_addindex!(lru, v, key)
+    lock(lru.lock) do
+        if _unsafe_haskey(lru, key)
+            _, n, s = lru.dict[key]
+            lru.currentsize -= s
+            s = lru.by(v)::Int
+            lru.currentsize += s
+            lru.dict[key] = (v, n, s)
+            _move_to_front!(lru.keyset, n)
+        else
+            _unsafe_addindex!(lru, v, key)
+        end
+        _unsafe_resize!(lru)
     end
-    _unsafe_resize!(lru)
-    unlock(lru.lock)
     return lru
 end
 
@@ -151,35 +145,35 @@ function _unsafe_resize!(lru::LRU, maxsize::Integer = lru.maxsize)
 end
 function Base.resize!(lru::LRU; maxsize::Integer = lru.maxsize)
     @assert 0 <= maxsize
-    lock(lru.lock)
-    _unsafe_resize!(lru, maxsize)
-    unlock(lru.lock)
+    lock(lru.lock) do
+        _unsafe_resize!(lru, maxsize)
+    end
     return lru
 end
 
 function Base.delete!(lru::LRU, key)
-    lock(lru.lock)
-    v, n, s = pop!(lru.dict, key)
-    lru.currentsize -= s
-    _delete!(lru.keyset, n)
-    unlock(lru.lock)
+    lock(lru.lock) do
+        v, n, s = pop!(lru.dict, key)
+        lru.currentsize -= s
+        _delete!(lru.keyset, n)
+    end
     return lru
 end
 function Base.pop!(lru::LRU, key)
-    lock(lru.lock)
-    v, n, s = pop!(lru.dict, key)
-    lru.currentsize -= s
-    _delete!(lru.keyset, n)
-    unlock(lru.lock)
-    return v
+    lock(lru.lock) do
+        v, n, s = pop!(lru.dict, key)
+        lru.currentsize -= s
+        _delete!(lru.keyset, n)
+        return v
+    end
 end
 
 function Base.empty!(lru::LRU)
-    lock(lru.lock)
-    lru.currentsize = 0
-    empty!(lru.dict)
-    empty!(lru.keyset)
-    unlock(lru.lock)
+    lock(lru.lock) do
+        lru.currentsize = 0
+        empty!(lru.dict)
+        empty!(lru.keyset)
+    end
     return lru
 end
 
