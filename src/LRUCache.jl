8f504b8b610934a7082dc2a2ac1cbd849881c25f
Jutho <Jutho@users.noreply.github.com>
2020-10-11 23:56:57 +0200

    Do not retain lock during `default()` call in `get(!)` (#19)
    * do not retain lock during default() call
    
    * bump version; update CI
    
    * actual working implementation
    
    * add test
    
    * prevent race condition in `get!` implementation
    
    Co-authored-by: Jutho Haegeman <jutho@Cirrus.local>

--- src/LRUCache.jl
+++ src/LRUCache.jl
@@ -63,14 +63,15 @@ function Base.get(lru::LRU, key, default)
     end
 end
 function Base.get(default::Callable, lru::LRU, key)
-    lock(lru.lock) do
-        if _unsafe_haskey(lru, key)
-            v = _unsafe_getindex(lru, key)
-            return v
-        else
-            return default()
-        end
+    lock(lru.lock)
+    if _unsafe_haskey(lru, key)
+        v = _unsafe_getindex(lru, key)
+        unlock(lru.lock)
+        return v
+    else
+        unlock(lru.lock)
     end
+    return default()
 end
 function Base.get!(lru::LRU, key, default)
     lock(lru.lock) do
@@ -85,16 +86,25 @@ function Base.get!(lru::LRU, key, default)
     end
 end
 function Base.get!(default::Callable, lru::LRU, key)
-    lock(lru.lock) do
-        if _unsafe_haskey(lru, key)
-            v = _unsafe_getindex(lru, key)
-            return v
-        end
-        v = default()
+    lock(lru.lock)
+    if _unsafe_haskey(lru, key)
+        v = _unsafe_getindex(lru, key)
+        unlock(lru.lock)
+        return v
+    else
+        unlock(lru.lock)
+    end
+    v = default()
+    lock(lru.lock)
+    if _unsafe_haskey(lru, key)
+        # should we test that this yields the same result as default()
+        v = _unsafe_getindex(lru, key)
+    else
         _unsafe_addindex!(lru, v, key)
         _unsafe_resize!(lru)
-        return v
     end
+    unlock(lru.lock)
+    return v
 end
 
 function _unsafe_getindex(lru::LRU, key)
