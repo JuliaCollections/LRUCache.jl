d83ba66ccb135fa34d862afe1b992f18677de245
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-01-14 23:59:18 +0100

    fix

--- src/LRUCache.jl
+++ src/LRUCache.jl
@@ -316,8 +316,8 @@ function _finalize_evictions!(finalizer, evictions)
     return
 end
 
-end # module
-
 if !isdefined(Base, :get_extension)
-  include("../ext/SerializationExt.jl")
+    include("../ext/SerializationExt.jl")
 end
+
+end # module
